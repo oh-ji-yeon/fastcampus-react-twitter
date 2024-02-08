bb8c9aa8a92a4d96ab52e843935af48dfa6a4cd5
oh-ji-yeon <o.delayed0106@gmail.com>
2024-02-08 10:22:37 +0900

    firestore로 데이터 생성하기 - 게시글 폼 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -54,11 +54,13 @@ const posts: PostProps[] = [
 export default function HomePage() {
     return (
         <div className="home">
-            <div className="home__title">Home</div>
-            <div className="home__tabs">
-                <div className="home__tab home__tab--active">For You</div>
-                <div className="home__tab">Following</div>
-            </div>
+            <div className="home__top">
+                <div className="home__title">Home</div>
+                <div className="home__tabs">
+                    <div className="home__tab home__tab--active">For You</div>
+                    <div className="home__tab">Following</div>
+                </div>
+        </div>
 
             {/* Post Form */}
             <PostForm />
