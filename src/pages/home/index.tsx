1a398ea1b0f3bc398050f1724f969e2c0e2532fd
oh-ji-yeon <o.delayed0106@gmail.com>
2024-02-08 13:58:57 +0900

    firestore로 실시간 데이터 가져오기 - onSnapShot

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,10 @@
+import { useContext, useEffect, useState } from "react";
+import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
+
 import PostForm from "components/posts/PostForm";
 import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import { db } from "firebaseApp";
 
 export interface PostProps {
     id: string;
@@ -13,45 +18,26 @@ export interface PostProps {
     comments?: any;
 }
 
-const posts: PostProps[] = [
-    {
-        id: "1",
-        email: "test@test.com",
-        content: "내용입니다.",
-        createdAt: "2024-02-02",
-        uid: "123123"
-    },
-    {
-        id: "2",
-        email: "test@test.com",
-        content: "내용입니다.",
-        createdAt: "2024-02-02",
-        uid: "123123"
-    },
-    {
-        id: "3",
-        email: "test@test.com",
-        content: "내용입니다.",
-        createdAt: "2024-02-02",
-        uid: "123123"
-    },
-    {
-        id: "4",
-        email: "test@test.com",
-        content: "내용입니다.",
-        createdAt: "2024-02-02",
-        uid: "123123"
-    },
-    {
-        id: "5",
-        email: "test@test.com",
-        content: "내용입니다.",
-        createdAt: "2024-02-02",
-        uid: "123123"
-    },
-];
-
 export default function HomePage() {
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        if(user) {
+            let postRef = collection(db, "posts"); //firestore에 posts 컬렉션을 가져와!
+            let postQuer = query(postRef, orderBy("createdAt", "desc"));
+
+            onSnapshot(postQuer, (snapShot) => {
+                let dataObj = snapShot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc?.id,
+                }));
+                // console.log(dataObj);
+                setPosts(dataObj as PostProps[]);
+            });
+        }
+    }, [user]);
+
     return (
         <div className="home">
             <div className="home__top">
@@ -67,9 +53,13 @@ export default function HomePage() {
 
             {/* Tweet Posts */}
             <div className="post">
-                {posts?.map((post) => (
+                {posts?.length > 0 ? posts?.map((post) => (
                     <PostBox post={post} key={post.id} />
-                ))}
+                )) : (
+                    <div className="post__no-posts">
+                        <div className="post__text">게시글이 없습니다.</div>
+                    </div>
+                )}
             </div>
         </div>
     );
