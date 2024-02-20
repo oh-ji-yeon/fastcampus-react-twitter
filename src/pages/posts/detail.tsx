738240d3c45b78665456c9b706c1e1d7684c0cd2
oh-ji-yeon <o.delayed0106@gmail.com>
2024-02-20 15:44:18 +0900

    firestore로 댓글 기능 구현

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,13 +1,15 @@
 import Loader from "components/loader/Loarder";
 import PostBox from "components/posts/PostBox";
 
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import PostHeader from "components/posts/PostHeader";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
 
 export default function PostDetail() {
     const params = useParams();
@@ -17,9 +19,13 @@ export default function PostDetail() {
     const getPost = useCallback(async() => {
         if(params.id) {
             const docRef = doc(db, "posts", params.id);
-            const docSnap = await getDoc(docRef);
+            // const docSnap = await getDoc(docRef);
 
-            setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+            onSnapshot(docRef, (doc) => {
+                setPost({...doc?.data() as PostProps, id: doc.id})
+            })
+
+            // setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
         }
     }, [params.id]);
 
@@ -30,7 +36,17 @@ export default function PostDetail() {
     return (
         <div className="post">
             <PostHeader />
-            {post ? <PostBox post={post} /> : <Loader />}
+            {post ? (
+                <>
+                <PostBox post={post} />
+                <CommentForm post={post} />
+                {post?.comments?.slice(0)?.reverse()?.map((data: CommentProps, index: number) => (
+                    <CommentBox data={data} key={index} post={post} />
+                ))}
+                </> 
+            )  : (
+                <Loader />
+            )}
         </div>
     )
 }
